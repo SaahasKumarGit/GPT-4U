518441817cac76516ea9c191686ce2e47627fb85
SaahasKumarGit <saahaskumar13@gmail.com>
2023-05-23 18:14:45 -0400

    initial commit - working chat interface

---
+++ webapp-launch.py
@@ -0,0 +1,21 @@
+import gradio as gr
+import random
+import time
+
+
+
+def respond(message, chat_history):
+        bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
+        chat_history.append((message, bot_message))
+        time.sleep(1)
+        return "", chat_history
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.Button("Clear")
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+    clear.click(lambda: None, None, chatbot, queue=False)
+
+demo.launch()
