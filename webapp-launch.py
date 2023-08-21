0a02b5060108402fa6bb18a093f040f5d6f722f7
SaahasKumarGit <saahaskumar13@gmail.com>
2023-08-20 20:44:34 -0400

    comments added and code updated

--- webapp-launch.py
+++ webapp-launch.py
@@ -2,24 +2,25 @@ import gradio as gr
 import webapp_functions
 import openai
 
+
 chat = [["This is you!", "This is the assistant!"]]
-conversations = webapp_functions.list_md_files()
 
 with gr.Blocks(theme=gr.themes.Soft()) as demo:
     with gr.Row():
-        with gr.Accordion(label= "Settings", open= False):
-            with gr.Row():
-                with gr.Column(scale = 1):
-                    modelSelection = gr.Dropdown(label="Model", choices= ["GPT 3.5 Turbo", "GPT 4"])
-                with gr.Column(scale = 1):
-                    apiKey = gr.Textbox(label="OpenAI API Key")
-        with gr.Accordion(label= "Analytics", open= False):
-            analytics = gr.Gallery()
-    chat_selection = gr.Dropdown(choices= conversations, allow_custom_value=True, label="Chat Selection")
-    chatbot = gr.Chatbot(value=chat)
-    msg = gr.Textbox(label="Prompt") 
+        with gr.Tab(label="Chat"):
+            chat_selection = gr.Dropdown(choices= webapp_functions.list_md_files(), allow_custom_value=True, label="Chat Selection")
+            chatbot = gr.Chatbot(value=chat)
+            msg = gr.Textbox(label="Prompt")  
+        with gr.Tab(label= "Settings"):
+            modelSelection = gr.Dropdown(label="Model", choices= ["GPT 3.5 Turbo - 16k", "GPT 4"])
+            apiKey = gr.Textbox(label="OpenAI API Key")
+            temperature = gr.Slider(minimum=0, maximum=1, value=0.7, label="Temperature",step=0.1, interactive=True)
+        with gr.Tab(label= "Analytics"):
+            usageGraph = gr.LinePlot()
+            analytics = gr.DataFrame()
 
-    msg.submit(webapp_functions.respond, [msg, chatbot,chat_selection, modelSelection, apiKey], [msg, chatbot])
+
+    msg.submit(webapp_functions.respond, [msg, chatbot,chat_selection, modelSelection, apiKey, temperature], [msg, chatbot])
     chat_selection.change(fn = webapp_functions.change_chat, inputs = chat_selection, outputs=chatbot)
 
 demo.launch(server_name="0.0.0.0")
--- webapp_functions.py
+++ webapp_functions.py
@@ -2,60 +2,77 @@ import os
 import random
 import time
 import openai
+import json
 
+# Code to list all files with the extension .txt.
+# Used for gradio to list all conversations from conversation dropdown.
+# Returns an array with each entry being the filename including the .txt extension
 def list_md_files():
-    return [f for f in os.listdir() if os.path.isfile(os.path.join( f)) and f != "README.md" and f.endswith('.txt')]
+    
+    #Finds all chats that end with the extension .chat
+    conversations = [filename for filename in os.listdir() if filename != "README.md" and filename.endswith('.chat')]
+    
+    # Strips the .chat extension from each file
+    for i in range (len(conversations)):
+        conversations[i] = conversations[i][:-5]
 
+    print(conversations)
+    return conversations
+
+# Returns a list containing question-answer pairs of the chat 
+# to the chatbot, so it can render it.
+# If the file is not found, it creates a new file.
+# DO NOT INCLUDE THE .chat EXTENSION!!!
+# The file will not be created here, if the file does not exist, it will
+# create it later when responding
 def change_chat(new_chat_name):
-    try:
-        file = open(new_chat_name, 'r')
+
+    # Opens the chat
+    try:    
+        new_chat_name += ".chat"
+        file = open(new_chat_name, 'r+')
         lines = file.readlines()
     
-        # Strip newline characters from each line
-        lines = [line.strip() for line in lines]
-        
-        # Group lines into question-answer pairs
+        # Group lines into question-answer pairs, required for the chatbot
         chat = [(lines[i], lines[i+1]) for i in range(0, len(lines), 2)]
-        
-        return chat
-    except FileNotFoundError:
-        file = open(new_chat_name, 'w+')
-        chat = []
         return chat
 
-def respond(message, chat_history, chatName, modelSelection, apiKey):
-        lines = ""
-        if(chatName == ""):
-            chatName = "DefaultChat.txt"
-            lines = ""
-            chat = []
-        else:
-            file = open(chatName, 'r')
-            lines = file.read()
-            file.close()
+    # If the file does nto exist, return an empty chat
+    except FileNotFoundError as error:
+        return []
+    
+
+# Given the setings, uses the openai api to generate a bot response
+# If the chatName does not exist, it is created
+def respond(message, chat_history, chatName, modelSelection, apiKey, temperature):
+
+        # Reads the conversation
+        chatName += ".chat"
+        file = open(chatName, 'a+')
+        lines = file.read()
+        lines += message
         
+        # Sets up the prompt
         openai.api_key = apiKey
+        lines+=message
+        OpenAPIMessage = [{"role" : "system", "content": lines}]
 
+        # Generates the response using the openai api
+        # if the key is invalide, 
+        response = ""
+        try:
+            if(modelSelection == "GPT 4"): 
+                response = openai.ChatCompletion.create(model = "gpt-4", messages = OpenAPIMessage, temperature = temperature) ["choices"][0]["message"]["content"]
+            else:
+                response = openai.ChatCompletion.create(model = "gpt-3.5-turbo-16k", messages = OpenAPIMessage, temperature = temperature) ["choices"][0]["message"]["content"]
+        except openai.error.AuthenticationError:
+                response = "Invalid OpenAI API key."
 
-        with open(chatName, 'a') as fOpen:
-            lines+=message
-            OpenAPIMessage = [{"role" : "system", "content": lines}]
-            try:
-                response = ""
-                if(modelSelection == "GPT 4"): 
-                    response = openai.ChatCompletion.create(model = "gpt-4", messages = OpenAPIMessage)
-                else:
-                    response = openai.ChatCompletion.create(model = "gpt-3.5-turbo", messages = OpenAPIMessage)
-            except openai.error.AuthenticationError:
-                bot_message = "Invalid OpenAI API key."
-                print("Invalid OpenAI API key.")
-                chat_history.append([message, bot_message])
-                return "", chat_history
-            bot_message = response["choices"][0]["message"]["content"]
-            chat_history.append([message, bot_message])
-            fOpen.write(message + '\n')
-            fOpen.write(bot_message + '\n')
+        # Appends the prompt/response to the chat_history to the file
+        # and appends it to chat history to be rendered by chatbot
+        chat_history.append([message, response])
+        file.write(message + '\n')
+        file.write(response + '\n')
+        
+        file.close()
         return "", chat_history
-
-def setAPIKey(apikey):
-    openai.api_key = apikey
